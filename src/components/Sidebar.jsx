4f5b96614a9c9912a559b01b7e52f5429fab75f3
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-09-27 18:56:29 +0530

    Added Redux Toolkit, Created Store and Slice

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-48 p-5 shadow-lg ">
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+        <li>YouTube Music</li>
+      </ul>
+      <h1 className="pt-5 font-bold ">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="pt-5 font-bold">Watch Later </h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
